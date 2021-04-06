9e5408de6e3b8a997eed6aa0e9d95b79b7c09bf5
shoneriki <ikirenohs@gmail.com>
2021-04-06 14:38:48 +0900

    vscode folder taken out

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -1,7 +1,7 @@
-youtuber = "me"
+verb = "panic"
 
 # print("subscribe to " + youtuber)
 # print("subscribe to {}".format(youtuber))
 # print(f"subscribe to {youtuber}")
 
-
+print(f"to {verb} or not to {verb}?")
