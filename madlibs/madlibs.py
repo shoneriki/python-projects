5cf7300f6481b97a97c7d6f0b09b9c3e49e0afa7
shoneriki <ikirenohs@gmail.com>
2021-04-06 14:47:06 +0900

    sample madlib works

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -1,7 +1,14 @@
-verb = "panic"
+adj = input("Adjective: ")
+adj2 = input("Another Adjective: ")
+verb = input("Verb: ")
+verb2 = input("Another Verb: ")
+person = input("Person's Name: ")
 
 # print("subscribe to " + youtuber)
 # print("subscribe to {}".format(youtuber))
 # print(f"subscribe to {youtuber}")
 
-print(f"to {verb} or not to {verb}?")
+madlib = f"Computer programming is so {adj}! It makes me very {adj2}  \
+  because I love to {verb}. Stay positive and {verb2} like {person}"
+
+print(madlib)
