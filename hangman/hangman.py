fd6d7b06787bf1b7fd18e25b81dae5f78b6b3c1b
shoneriki <ikirenohs@gmail.com>
2021-04-08 19:36:03 +0900

    guessed letter functionality

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,30 +1,40 @@
 # tutorial of  kying18
 import random
 from words import words
+import string
 
 def get_valid_word(words):
   word = random.choice(words)
   while '-' in word or ' ' in word:
     word = random.choice(words)
 
-  return word
+  return word.upper()
 
 def hangman():
   word = get_valid_word(words)
   word_letters = set(word)
   alphabet = set(string.ascii_uppercase)
   used_letters = set()
+  
+  while len(word_letters) > 0:
+    # letters used
+    # ' '.join(['a', 'b', 'c']) --> 'a b c'
+    print('You have used these letters ', ' '.join(used_letters))
 
-  user_letter = input('Guess a letter: ').upper()
-  if user_letter in alphabet - used_letters:
-    used_letters.add(user_letter)
-    if user_letter in word_letters:
-      word_letters.remove(user_letter)
+    # what current word is (ie W - A T)
+    word_list = [letter if letter in used_letters else '-' for letter in word]
+    print('Current word: ', ' '.join(word_list))
 
-  elif user_letter in used_letters:
-    print('You have already used that character. Try again.')
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+      used_letters.add(user_letter)
+      if user_letter in word_letters:
+        word_letters.remove(user_letter)
 
-  else:
-    print('Invalid character. Try again.')
+    elif user_letter in used_letters:
+      print('You have already used that character. Try again.')
 
-user_input = input('Type something: ')
+    else:
+      print('Invalid character. Try again.')
+
+hangman()
