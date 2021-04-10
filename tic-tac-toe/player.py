a29de170db2c4339c3db4d5d5f6f8df163c5032c
shoneriki <ikirenohs@gmail.com>
2021-04-10 16:43:25 +0900

    initial player file

---
+++ tic-tac-toe/player.py
@@ -0,0 +1,25 @@
+import math
+import random
+
+class Player:
+  def __init__(self, letter):
+  # letter = x or o
+  self.letter = letter
+
+  # next move
+  def get_move(self, game):
+    pass
+
+class RandomComputerPlayer(Player):
+  def __init__(self, letter):
+    super().__init__(letter)
+
+  def get_move(self, game):
+    pass
+
+class HumanPlayer(Player):
+  def __init__(self, letter):
+    super().__init__(letter)
+
+  def get_move(self, game):
+    pass
